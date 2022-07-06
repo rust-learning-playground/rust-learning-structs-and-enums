f3eafe0da707710a5591e5771bc08a15a0c9ff06
Sergio Arroutbi <sarroutb@redhat.com>
2022-07-06 15:55:31 +0200

    Structs and enums
    Signed-off-by: Sergio Arroutbi <sarroutb@redhat.com>

---
+++ src/main.rs
@@ -0,0 +1,54 @@
+use strum::IntoEnumIterator;
+use strum_macros::EnumIter;
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+#[derive(Debug, EnumIter)]
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
+
+fn value_in_cents(coin: &Coin) -> u8 {
+    match coin {
+	Coin::Penny => {
+	    println!("Lucky Penny:{:?}", coin);
+	    1
+	}
+	Coin::Nickel => 5,
+	Coin::Dime => 10,
+	Coin::Quarter => 25,
+    }
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+	self.width * self.height
+    }
+}
+
+fn enum_coins() {
+    for k in Coin::iter() {
+	println!("{:?} value:{} cts.", k, value_in_cents(&k));
+    }
+}
+
+fn rect_area() {
+    let r = Rectangle{width:10, height:20};
+    println!("Rectangle({},{}):{:?}->area:{}", r.width, r.height, r, r.area());
+}
+
+fn go() {
+    rect_area();
+    enum_coins();
+}
+
+fn main() {
+    go();
+}
